e0fa575ba6d7fabe753c6a5e637ea3417db757a9
thuanpm <thuanpm@sunshinegroup.vn>
2022-01-08 17:28:03 +0700

    init project

---
+++ src/main/java/com/microservice/springnodehttp/SpringnodehttpApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.springnodehttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringnodehttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringnodehttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/springnodehttp/SpringnodehttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.springnodehttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringnodehttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
